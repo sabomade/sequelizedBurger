f6511c51dd7e40becf5c42555d75e8ef6a9d8dae
Sara Bolduc <sara.s.bolduc@gmail.com>
2020-01-20 17:23:08 -0800

    adds initial files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Mushroom Burger');
+INSERT INTO burgers (burger_name) VALUES ('Vegan BBQ Patti');
+INSERT INTO burgers (burger_name) VALUES ('Chickn Bacn Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Deconstructed Burger Salad', true);
+
---
+++ server.js
@@ -0,0 +1,7 @@
+// DEPENDENCIES
+// ===========================
+var express = require("express");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
